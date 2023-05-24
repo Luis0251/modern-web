4588687a1874adac4102d928282c49e9a58dcc07
Luis0251 <luis0251@hotmail.com>
2023-05-24 14:53:34 -0500

    feature: Brand component finished

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,26 @@
-
-
+import {google,
+  dropbox,
+  shopify,
+  slack,
+  atlassian} from './imports'
+import './brand.css'
 export const Brand = () => {
+  const images = [
+    { src: google, alt: 'google' },
+    { src: dropbox, alt: 'dropbox' },
+    { src: shopify, alt: 'shopify' },
+    { src: slack, alt: 'slack' },
+    { src: atlassian, alt: 'atlassian' }
+  ];
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+       {
+        images.map((image,i)=>(
+          <div key={i}>
+            <img src={image.src} alt={image.alt} />
+          </div>
+        ))
+       }
+    </div>
   )
 }
