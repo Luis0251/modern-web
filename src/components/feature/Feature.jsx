efeae9e63f8c14e20c141c3c5f65291eef52697e
Luis0251 <luis0251@hotmail.com>
2023-05-25 11:08:23 -0500

    fix:changes this component props

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,23 +1,26 @@
 import PropTypes from 'prop-types';
 import './feature.css'
 
-export const Feature = ({content}) => {
-  const {title,text} = content
+export const Feature = ({title,text, content}) => {
+  const featureTitle = content ? content.title : title;
+  const featureText = content ? content.text : text;
   return (
     <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
       <div />
-      <h1>{title}</h1>
+      <h1>{featureTitle}</h1>
       </div>
       <div className="gpt3__features-container_feature-text">
-       <p>{text}</p> 
+       <p>{featureText}</p> 
       </div>
     </div>
   )
 }
 Feature.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
   content: PropTypes.shape({
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
-  }).isRequired,
+  }),
 };
